917e5677c95b99c998c1dc964dbb216b4c0c5c82
lyh95 <13269577669@163.com>
2018-08-23 16:17:34 +0800

    ZBdataService first

--- acmrinput30/src/com/acmr/service/zbdata/ZBdataService.java
+++ acmrinput30/src/com/acmr/service/zbdata/ZBdataService.java
@@ -1,4 +1,13 @@
 package com.acmr.service.zbdata;
 
+import acmr.cubequery.service.cubequery.entity.CubeNode;
+
+import java.util.ArrayList;
+
 public class ZBdataService {
+    OriginService origin=new OriginService();
+    public ArrayList<CubeNode> getZB(){
+        ArrayList<CubeNode> zb=origin.getwdsubnodes("zb","");
+        return zb;
+    }
 }
