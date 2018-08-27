892d241e4f3d87c7558e095523720209cfd02568
lyh95 <13269577669@163.com>
2018-08-27 18:04:11 +0800

    getindexlist

--- acmrinput30/src/com/acmr/dao/oracle/zhzs/OraIndexListDaoImpl.java
+++ acmrinput30/src/com/acmr/dao/oracle/zhzs/OraIndexListDaoImpl.java
@@ -1,7 +1,10 @@
 package com.acmr.dao.oracle.zhzs;
 
+import acmr.util.DataTable;
 import com.acmr.dao.AcmrInputDPFactor;
 import com.acmr.dao.zhzs.IIndexListDao;
+
+import java.util.List;
 
 public class OraIndexListDaoImpl implements IIndexListDao {
 
@@ -9,5 +12,11 @@ public class OraIndexListDaoImpl implements IIndexListDao {
     public String getName() {
         String sql="select cname from tb_coindex_index where code = 'D002' ";
         return AcmrInputDPFactor.getQuickQuery().getDataScarSql(sql);
+    }
+
+    @Override
+    public DataTable getCodeByUser(String usercode) {
+        String sql="select * from tb_coindex_index where createuser= ? ";
+        return AcmrInputDPFactor.getQuickQuery().getDataTableSql(sql,new Object[] {usercode});
     }
 }
