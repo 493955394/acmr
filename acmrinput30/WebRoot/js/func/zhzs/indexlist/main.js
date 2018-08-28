b6ed8a41910976f08435d285a4d646155c94b643
lyh95 <13269577669@163.com>
2018-08-28 11:11:51 +0800

    lost main

--- acmrinput30/WebRoot/js/func/zhzs/indexlist/main.js
+++ acmrinput30/WebRoot/js/func/zhzs/indexlist/main.js
@@ -1,10 +1,13 @@
 define(function (require,exports,module) {
-    console.log("引入main,js")
     'use strict';
     var $ = require('jquery'),
-        tree = require('tree');
-        modal = require('modal');
+        tree = require('tree'),
+        modal = require('modal'),
+        listjsp= require('listjsp');
     var setting = {
+        async:{
+
+        },
         data: {
             simpleData: {
                 enable: true
@@ -13,36 +16,15 @@ define(function (require,exports,module) {
     };
 
     var zNodes =[
-        { id:1, pId:0, name:"父节点1 - 展开", open:true},
-        { id:11, pId:1, name:"父节点11 - 折叠"},
-        { id:111, pId:11, name:"叶子节点111"},
-        { id:112, pId:11, name:"叶子节点112"},
-        { id:113, pId:11, name:"叶子节点113"},
-        { id:114, pId:11, name:"叶子节点114"},
-        { id:12, pId:1, name:"父节点12 - 折叠"},
-        { id:121, pId:12, name:"叶子节点121"},
-        { id:122, pId:12, name:"叶子节点122"},
-        { id:123, pId:12, name:"叶子节点123"},
-        { id:124, pId:12, name:"叶子节点124"},
-        { id:13, pId:1, name:"父节点13 - 没有子节点", isParent:true},
-        { id:2, pId:0, name:"父节点2 - 折叠"},
-        { id:21, pId:2, name:"父节点21 - 展开", open:true},
-        { id:211, pId:21, name:"叶子节点211"},
-        { id:212, pId:21, name:"叶子节点212"},
-        { id:213, pId:21, name:"叶子节点213"},
-        { id:214, pId:21, name:"叶子节点214"},
-        { id:22, pId:2, name:"父节点22 - 折叠"},
-        { id:221, pId:22, name:"叶子节点221"},
-        { id:222, pId:22, name:"叶子节点222"},
-        { id:223, pId:22, name:"叶子节点223"},
-        { id:224, pId:22, name:"叶子节点224"},
-        { id:23, pId:2, name:"父节点23 - 折叠"},
-        { id:231, pId:23, name:"叶子节点231"},
-        { id:232, pId:23, name:"叶子节点232"},
-        { id:233, pId:23, name:"叶子节点233"},
-        { id:234, pId:23, name:"叶子节点234"},
-        { id:3, pId:0, name:"父节点3 - 没有子节点", isParent:true}
+        { id:"#1", pId:0, name:"指数",isParent:true},
+        { id:"#2", pId:0, name:"我收到的指数",isParent:true},
+        { id:"#3", pId:0, name:"我共享的指数", isParent:true},
     ];
+    var indexlist=listjsp.indexlist;
+    console.log(indexlist)
+    for(var i=0;i<indexlist.length;i++){
+        zNodes.push(indexlist[i])
+    }
 
     $(document).ready(function(){
         $.fn.zTree.init($("#treeDemo"), setting, zNodes);
