dbfdb409e5395c9aaddb1713c0a899b9c22bfb98
unknown <493955394@qq.com>
2018-09-05 16:52:27 +0800

    新增暂时跳转新页面

--- acmrinput30/WebRoot/js/func/zhzs/zsjhEdit/module.js
+++ acmrinput30/WebRoot/js/func/zhzs/zsjhEdit/module.js
@@ -168,37 +168,9 @@ define(function (require,exports,module) {
                     alert("指标下不能添加内容！");
                     return;
                 }else{
-
+                    window.location.href=common.rootPath+'zbdata/zsjhedit.htm?m=toAdd&procodeId='+choosedprocode+'&procodeName='+choosedname+'&indexCode='+indexCode;
                 }
             }
         })
-    });
-    /**
-     * 新增模型节点
-     */
-    $(document).on('submit', '.J_addZS_form', function(event) {
-        event.preventDefault();
-        var self = this,
-            currentUrl = $(self).attr('action');
-        $.ajax({
-            url: currentUrl,
-            data: $(self).serialize(),
-            type: 'post',
-            dataType: 'json',
-            timeout: 10000,
-            success: function(data) {
-                if (data.returncode == 200) {
-                    alert("保存成功");
-                }else {
-                    alert("保存失败");
-                }
-            },
-            error: function() {
-                common.commonTips('添加失败');
-
-            }
-
-        })
-
     });
 })
--- acmrinput30/WebRoot/js/func/zhzs/zsjhEdit/toAdd.js
+++ acmrinput30/WebRoot/js/func/zhzs/zsjhEdit/toAdd.js
@@ -34,4 +34,62 @@ define(function (require,exports,module) {
         })
 
     });
+    /**
+     * 点击选择按钮之后隐藏和显示的内容
+     */
+    $(document).ready(function(){
+        var obj, index, id;
+        obj = document.getElementById('selectifzs');
+        index = obj.selectedIndex;
+        id = obj.options[index].value;
+        if(id == 1){
+            $('#secend_zs').show();
+        }
+        else if(id == 0){
+            $('#select_zb').show();
+        }
+    })
+    $(document).on('change', '[name=ifzs]', function(event){
+        var isGroup = $(this).val();
+        console.log(isGroup)
+        if(isGroup === '1'){
+            $('#select_zb').hide();
+            $('#secend_zs').show();
+        }else if(isGroup === '0'){
+            //clearContent();
+            $('#secend_zs').hide();
+            $('#select_zb').show();
+        }else {
+            $('#select_zb').hide();
+            $('#secend_zs').hide();
+        }
+    })
+    /**
+     * 新增模型节点
+     */
+    $(document).on('submit', '.J_addZS_form', function(event) {
+        event.preventDefault();
+        var self = this,
+            currentUrl = $(self).attr('action');
+        $.ajax({
+            url: currentUrl,
+            data: $(self).serialize(),
+            type: 'post',
+            dataType: 'json',
+            timeout: 10000,
+            success: function(data) {
+                if (data.returncode == 200) {
+                    alert("保存成功");
+                }else {
+                    alert("保存失败");
+                }
+            },
+            error: function() {
+                common.commonTips('添加失败');
+
+            }
+
+        })
+
+    });
 })
